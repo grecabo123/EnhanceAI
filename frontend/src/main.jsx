91628e8d18a532e05e844e7deec3c753759b3419
GCodingIsLife <artamay1@gmail.com>
2024-08-26 21:05:44 +0800

    changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './app.css'
+import './App.css'
 import { Ripple } from 'primereact/ripple';
 import {BrowserRouter as Router } from 'react-router-dom'
 
