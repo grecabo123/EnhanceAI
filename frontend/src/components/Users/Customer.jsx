849b8174654b8ecdc0f3d96546fe2efbb3199d39
GCodingIsLife <artamay1@gmail.com>
2024-08-31 00:57:29 +0800

    changes

--- frontend/src/components/Users/Customer.jsx
+++ frontend/src/components/Users/Customer.jsx
@@ -174,9 +174,6 @@ function Admin() {
                                                         <li className="sidebar-item">
                                                             <Link to="/customer/buyer/history" className="sidebar-link"><span className='child'>History Buyer</span></Link>
                                                         </li>
-                                                        {/* <li className="sidebar-item">
-                                                            <Link to="/customer/purchase/generate/design" className="sidebar-link"><span className='child'>Feedback</span></Link>
-                                                        </li> */}
                                                     </ul>
                                                 </li>
 
@@ -241,124 +238,133 @@ function Admin() {
                             </main>
 
                             <Sidebar style={{ backgroundColor: "#212529" }} visible={visible} onHide={() => setVisible(false)}>
-                                <div className="h-100">
-                                    <div className="sidebar-logo d-flex justify-content-center align-items-center">
-                                        <img src={admin} alt="" width={100} style={{ borderRadius: "50%" }} />
-                                    </div>
-                                    <div className="mt-2 text-center">
-                                        <h5 className='text-light'>CUSTOMER FLOWER</h5>
-                                    </div>
-                                    <ul className="sidebar-nav">
-                                        <li className="sidebar-header">
-                                            Pages
-                                        </li>
-                                        <li className="sidebar-item">
-                                            <a href="/admin" className="sidebar-link">
-                                                <FcHome size={20} className='align-middle' />
-                                                <span>Dashboard</span>
-                                            </a>
-                                        </li>
-
-                                        <li className="sidebar-header">
-                                            Find Products
-                                        </li>
-                                        <li className="sidebar-item">
-                                            <Link to="/customer/product/search" className="sidebar-link">
-                                                <FcSearch size={20} className='align-middle' />
-                                                <span>Search Product</span>
-                                            </Link>
-                                        </li>
-                                        <li className="sidebar-item">
-                                            <Link to="/customer/purchase/generate/design" className="sidebar-link">
-                                                <FaRobot size={20} className='align-middle' />
-                                                <span>Generate Design</span>
-                                            </Link>
-                                        </li>
-                                        <li className="sidebar-item">
-                                            <Link to="/customer/product/order" className="sidebar-link">
-                                                <FaCartPlus size={20} className='align-middle' />
-                                                <span>Order Status</span>
-                                            </Link>
-                                        </li>
-
-                                        {
-                                            UserControl.shop_status == 1 ? "" :
-                                                <>
-                                                    <li className="sidebar-header">
-                                                        Build a Shop
-                                                    </li>
-                                                    <li className="sidebar-item">
-                                                        <Link to="/customer/request/form" className="sidebar-link">
-                                                            <FcFile size={20} className='align-middle' />
-                                                            <span>Request Form</span>
-                                                        </Link>
-                                                    </li>
-                                                </>
-                                        }
-
-                                        {
-
-                                            UserControl.shop_status === null ? "" : UserControl.shop_status === 0 ? "" :
-
-                                                <React.Fragment>
-                                                    <li className="sidebar-header">
-                                                        Products
-                                                    </li>
-                                                    <li className="sidebar-item">
-                                                        <a href="#" className="sidebar-link collapsed" data-bs-target="#sells" data-bs-toggle="collapse"
-                                                            aria-expanded="false">
-                                                            <FcFolder size={20} className='align-middle' />
-                                                            <span>Inventory</span>
-                                                        </a>
-                                                        <ul id="sells" className="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                                                            <li className="sidebar-item">
-                                                                <Link to="/customer/product/status" className="sidebar-link"><span className='child'>Add Products</span></Link>
-                                                            </li>
-                                                            <li className="sidebar-item">
-                                                                <Link to="/customer/product/design" className="sidebar-link"><span className='child'>Add Designs</span></Link>
-                                                            </li>
-
-                                                        </ul>
-                                                    </li>
-
-                                                    <li className="sidebar-header">
-                                                        List of Buyer
-                                                    </li>
-                                                    <li className="sidebar-item">
-                                                        <a href="#" className="sidebar-link collapsed" data-bs-target="#buyer" data-bs-toggle="collapse"
-                                                            aria-expanded="false">
-                                                            <FaUsers size={20} className='align-middle' />
-                                                            <span>Buyers</span>
-                                                        </a>
-                                                        <ul id="buyer" className="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                                                            <li className="sidebar-item">
-                                                                <Link to="/customer/buyer/list" className="sidebar-link"><span className='child'>Buyers</span></Link>
-                                                            </li>
-                                                            <li className="sidebar-item">
-                                                                <Link to="/customer/buyer/history" className="sidebar-link"><span className='child'>History Buyer</span></Link>
-                                                            </li>
-                                                            {/* <li className="sidebar-item">
-                                                                <Link to="/customer/purchase/generate/design" className="sidebar-link"><span className='child'>Feedback</span></Link>
-                                                            </li> */}
-                                                        </ul>
-                                                    </li>
-
-                                                </React.Fragment>
-
-
-                                        }
-                                        <li className="sidebar-header">
-                                            History
-                                        </li>
-                                        <li className="sidebar-item">
-                                            <Link to="/customer/history/logs" className="sidebar-link">
-                                                <FaDesktop size={20} className='align-middle' />
-                                                <span>Activity Logs</span>
-                                            </Link>
-                                        </li>
-
-                                    </ul>
+                            <div className="h-100">
+                                <div className="sidebar-logo d-flex justify-content-center align-items-center">
+                                    {
+                                        UserControl.shop_logo == null ? 
+                                        <img src={admin} alt="" width={100} style={{ borderRadius: "50%" }} /> 
+                                        :
+                                    <img src={`${import.meta.env.VITE_API_BASE_URL}/${UserControl.shop_logo}`} width={100} style={{ borderRadius: "50%" }} alt="" />
+                                    }
                                 </div>
+                                <div className="mt-2 text-center">
+                                    <h5 className='text-light'>{UserControl.shop_status == 1 ? UserControl.shop_name : "Customer"}</h5>
+                                </div>
+                                <ul className="sidebar-nav">
+                                    <li className="sidebar-header">
+                                        Pages
+                                    </li>
+                                    <li className="sidebar-item">
+                                        <a href="/customer" className="sidebar-link">
+                                            <FcHome size={20} className='align-middle' />
+                                            <span>Dashboard</span>
+                                        </a>
+                                    </li>
+
+                                    <li className="sidebar-header">
+                                        Find Products
+                                    </li>
+                                    <li className="sidebar-item">
+                                        <Link to="/customer/product/search" className="sidebar-link">
+                                            <FcSearch size={20} className='align-middle' />
+                                            <span>Search Product</span>
+                                        </Link>
+                                    </li>
+                                    <li className="sidebar-item">
+                                        <Link to="/customer/visit/shop" className="sidebar-link">
+                                            <FcShop size={20} className='align-middle' />
+                                            <span>Visit Shop</span>
+                                        </Link>
+                                    </li>
+                                    <li className="sidebar-item">
+                                        <Link to="/customer/purchase/generate/design" className="sidebar-link">
+                                            <FaRobot size={20} className='align-middle' />
+                                            <span>Generate Design</span>
+                                        </Link>
+                                    </li>
+                                    <li className="sidebar-item">
+                                        <Link to="/customer/product/order" className="sidebar-link">
+                                            <FaCartPlus size={20} className='align-middle' />
+                                            <span>Order Status</span>
+                                        </Link>
+                                    </li>
+
+                                    {
+                                        UserControl.shop_status == 1 ? "" :
+                                            <>
+                                                <li className="sidebar-header">
+                                                    Build a Shop
+                                                </li>
+                                                <li className="sidebar-item">
+                                                    <Link to="/customer/request/form" className="sidebar-link">
+                                                        <FcFile size={20} className='align-middle' />
+                                                        <span>Request Form</span>
+                                                    </Link>
+                                                </li>
+                                            </>
+                                    }
+
+                                    {
+
+                                        UserControl.shop_status === null ? "" : UserControl.shop_status === 0 ? "" :
+
+                                            <React.Fragment>
+                                                <li className="sidebar-header">
+                                                    Products
+                                                </li>
+                                                <li className="sidebar-item">
+                                                    <a href="#" className="sidebar-link collapsed" data-bs-target="#sells" data-bs-toggle="collapse"
+                                                        aria-expanded="false">
+                                                        <FcFolder size={20} className='align-middle' />
+                                                        <span>Inventory</span>
+                                                    </a>
+                                                    <ul id="sells" className="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                                                        <li className="sidebar-item">
+                                                            <Link to="/customer/product/status" className="sidebar-link"><span className='child'>Add Products</span></Link>
+                                                        </li>
+                                                        <li className="sidebar-item">
+                                                            <Link to="/customer/product/design" className="sidebar-link"><span className='child'>Add Designs</span></Link>
+                                                        </li>
+
+                                                    </ul>
+                                                </li>
+
+                                                <li className="sidebar-header">
+                                                    List of Buyer
+                                                </li>
+                                                <li className="sidebar-item">
+                                                    <a href="#" className="sidebar-link collapsed" data-bs-target="#buyer" data-bs-toggle="collapse"
+                                                        aria-expanded="false">
+                                                        <FaUsers size={20} className='align-middle' />
+                                                        <span>Buyers</span>
+                                                    </a>
+                                                    <ul id="buyer" className="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                                                        <li className="sidebar-item">
+                                                            <Link to="/customer/buyer/list" className="sidebar-link"><span className='child'>New Order</span></Link>
+                                                        </li>
+                                                        <li className="sidebar-item">
+                                                            <Link to="/customer/buyer/approve" className="sidebar-link"><span className='child'>Approve Order</span></Link>
+                                                        </li>
+                                                        <li className="sidebar-item">
+                                                            <Link to="/customer/buyer/history" className="sidebar-link"><span className='child'>History Buyer</span></Link>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+
+                                            </React.Fragment>
+                                    }
+                                    <li className="sidebar-header">
+                                        History
+                                    </li>
+                                    <li className="sidebar-item">
+                                        <Link to="/customer/history/logs" className="sidebar-link">
+                                            <FaDesktop size={20} className='align-middle' />
+                                            <span>Activity Logs</span>
+                                        </Link>
+                                    </li>
+
+                                </ul>
+                            </div>
 
                             </Sidebar>
 
